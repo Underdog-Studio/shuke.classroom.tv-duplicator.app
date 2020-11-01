e59d8270d80e4ef12d8bd223aacbc7244e695508
zhymc <854185073@qq.com>
2020-11-01 19:01:25 +0800

    fix: fix a relative directory problem

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const {fork} = require("child_process");
 const assert = require("assert");
+const Path = require("path");
 let roomid = process.argv[2];
 let tvname = process.argv[3];
 assert(roomid);
 assert(tvname);
 
-fork("./client/client.js",[roomid,tvname]);
-fork("./server/server.js",[roomid,tvname]);
+fork(Path.join(__dirname ,"/client/client.js"),[roomid,tvname]);
+fork(Path.join(__dirname ,"/server/server.js"),[roomid,tvname]);
